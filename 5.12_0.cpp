cf4c4ca8d90745e6a51a702cbacb4ffa893c664a
Felix-Yang887 <felix@example.com>
2026-05-12 20:09:36 +0800

    fix

--- 5.12_0.cpp
+++ 5.12_0.cpp
@@ -1,89 +1,73 @@
-// 数据结构：用dp[i][j]表示从s[i]到s[j]的最长回文字符串的长度
-#include <bits/stdc++.h>
-using namespace std;
-int Max(int a, int b)
-{
-	if (a >= b)
-		return a;
-	else
-		return b;
-}
+// 最长回文子序列（Longest Palindromic Subsequence）
+// dp[i][j] 表示 s[i..j] 中最长回文子序列的长度
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+
+const int MAXN = 1001;
+
+int dp[MAXN][MAXN];
+
 int main()
 {
-	char s[1000];
+	char s[MAXN];
 	fgets(s, sizeof(s), stdin);
 	s[strcspn(s, "\n")] = '\0';
+	int n = strlen(s);
 
-	int dp[1000][1000];
-	for (int l = 1; l <= strlen(s); l++)
+	for (int l = 1; l <= n; l++)
 	{
-		for (int i = 0; i <= strlen(s) - l; i++)
+		for (int i = 0; i <= n - l; i++)
 		{
-			int j = l + i - 1;
+			int j = i + l - 1;
 			if (l == 1)
 			{
 				dp[i][j] = 1;
 			}
+			else if (l == 2)
+			{
+				dp[i][j] = (s[i] == s[j]) ? 2 : 1;
+			}
 			else
 			{
-				if (s[i] == s[j] && l == 2)
-					dp[i][j] = 2;
-				else if (s[i] == s[j])
+				if (s[i] == s[j])
 					dp[i][j] = dp[i + 1][j - 1] + 2;
-				else if (l == 2)
-					dp[i][j] = 1;
 				else
-					dp[i][j] = Max(dp[i + 1][j], dp[i][j - 1]);
+					dp[i][j] = std::max(dp[i + 1][j], dp[i][j - 1]);
 			}
 		}
 	}
 
-	// 回溯
-	char LPS[501], LPS1[501];
-	int tmp = 0;
-	int left = 0, right = strlen(s) - 1;
+	// 回溯输出最长回文子序列
+	char lps[MAXN];
+	int len = 0;
+	int left = 0, right = n - 1;
 	while (left <= right)
 	{
 		if (s[left] == s[right])
 		{
-			LPS[tmp++] = s[left];
+			lps[len++] = s[left];
 			left++;
 			right--;
 		}
 		else
 		{
 			if (dp[left + 1][right] >= dp[left][right - 1])
-			{
 				left++;
-			}
 			else
 				right--;
 		}
 	}
-	LPS[tmp] = '\0';
-	if (left == right + 1)
-	{
-		// 偶数
-		for (int i = 0; i < strlen(LPS); i++)
-		{
-			printf("%c", LPS[i]);
-		}
-		for (int i = strlen(LPS) - 1; i >= 0; i--)
-		{
-			printf("%c", LPS[i]);
-		}
-	}
-	if (left == right + 2)
-	{
-		// 奇数
-		for (int i = 0; i < strlen(LPS); i++)
-		{
-			printf("%c", LPS[i]);
-		}
-		for (int i = strlen(LPS) - 2; i >= 0; i--)
-		{
-			printf("%c", LPS[i]);
-		}
-	}
+
+	for (int i = 0; i < len; i++)
+		putchar(lps[i]);
+	if (left == right + 2) // 奇数长度，中间字符只输出一次
+		for (int i = len - 2; i >= 0; i--)
+			putchar(lps[i]);
+	else // 偶数长度
+		for (int i = len - 1; i >= 0; i--)
+			putchar(lps[i]);
+	putchar('\n');
+
 	return 0;
 }
